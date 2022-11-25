e8c7d2329b0534cc2013d65d0687d498608297f9
pk558-wq <pk558@njit.edu>
2022-11-25 13:03:26 -0500

    fetch faers products recalls from fda api

--- project/html/php/fetch_fda_recalls.php
+++ project/html/php/fetch_fda_recalls.php
@@ -1,32 +1,32 @@
 <?php
 include_once 'db_util.php';
 include_once 'create_fda_tables.php';
-include_once 'config.php';
+include_once "config.php";
 
-function fetch_ndc($skip, $limit) {
-    $endpoint = "https://api.fda.gov/drug/ndc.json";
-    $search_by="finished:true";
-    $params = array('api_key' => FDA_API_KEY, 'search' => $search_by, 'limit'=> $limit);
+function fetch_recalls_by_dates($skip, $today, $limit) {
+    $endpoint = "https://api.fda.gov/drug/enforcement.json";
+
+    $url = $endpoint."?search=report_date:[".FDA_RECALL_START_DATE."+TO+".$today."]&limit=".$limit."&api_key=".FDA_API_KEY;
     if ($skip > 0) {
-	    $params['skip'] = $skip;
+	    $url = $url."&skip=".$skip;
     }
-    $url = $endpoint . '?' . http_build_query($params);
 
+    print($url);
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, $url);
 
     curl_setopt($ch, CURLOPT_HEADER, 0);
 
+    // Return response instead of outputting
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+    // Execute the GET request
     $result = curl_exec($ch);
-    $info = curl_getinfo($ch);
-    print_r($info);
 
     if(curl_error($ch)) {
-	    echo(curl_error($ch));
-	    $rc = 1;
+        echo(curl_error($ch));
+        $rc = 1;
     } else {
 	    $rc = 0;
     }
@@ -42,10 +42,11 @@ $skip = 0;
 $limit = 200;
 $conn = connect_to_db();
 
+$today = date("Ymd");
 while(true) {
     echo "Total records processed " . $skip;
 
-    list('rc' => $rc, 'data' => $obj) = fetch_ndc($skip, $limit);
+    list('rc' => $rc, 'data' => $obj) = fetch_recalls_by_dates($skip, $today, $limit);
     if ($rc != 0) {
 	    echo "Sync failed ";
 	    return;
@@ -69,16 +70,27 @@ while(true) {
     $skip = $skip + $current_len;
 
     foreach($objects as $elem)  {
-      insert_product($conn, $elem['product_ndc'], $elem['generic_name'], $elem['brand_name'],$elem['labeler_name']);
+	if (!array_key_exists('openfda', $elem)) {
+		continue;
+	}
+
+	$openfda = $elem['openfda'];
+
+	if (!array_key_exists('product_ndc', $openfda)) {
+		continue;
+	}
+
+        insert_recalls($conn, $elem['recall_number'], $elem['reason_for_recall'], $elem['report_date'], $elem['recalling_firm'], $elem['status']);
+
+        foreach ($openfda['product_ndc'] as $pr_ndc) {
+             insert_product_recalls($conn, $pr_ndc, $elem['recall_number']);
+        }
     }
 
     if($current_len < $limit) {
 	    break;
     }
     echo "Total records processed " . $skip;
-    if ($skip >= MAX_FDA_PRODUCTS) {
-      break;
-    }
 }
 
 close_db($conn);
